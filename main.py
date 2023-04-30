5576ff10f7acc55bef7f922b2e5b51b987e69d58
kidavspb <kidavspb2002@gmail.com>
2023-04-30 22:57:58 +0300

    Считается время, есть отмена, покрасивше

--- main.py
+++ main.py
@@ -1,53 +1,35 @@
 import telebot
-import datetime
-import time
-from docx import Document
-import subprocess
 from pdf2image import convert_from_path
 from PIL import Image
 import time
 import os
-import traceback
 import logging
+from utils import *
 from secret_file import *
 
 logging.basicConfig(
     level=logging.INFO,
     #    filename = "MGlog.log",
-    format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s, user: %(user)s",
+    format="%(asctime)s - %(module)s - %(levelname)s: %(lineno)d - %(message)s",
     handlers=[
         logging.FileHandler("MGlog.log"),
         logging.StreamHandler()
     ],
-    datefmt='%H:%M:%S',
+    datefmt='%d/%b %H:%M:%S',
 )
+
+bot = telebot.TeleBot(TOKEN)
 
 while True:
     try:
-        logging.info("Bot running..", extra={"user": 0})
-        months = ["января", "февраля",
-                  "марта", "апреля", "мая",
-                  "июня", "июля", "августа",
-                  "сентября", "октября", "ноября",
-                  "декабря"]
-        bot = telebot.TeleBot(TOKEN)
-
-
         @bot.message_handler(commands=['start'])
         def welcome_message(message):
-            logging.info("Start message from user", extra={"user": message.from_user.username})
+            logging.info(f"Start message from user @{message.from_user.username}")
             bot.send_message(message.chat.id, "Здравствуйте! Пожалуйста, выберите тип справки:\n\n"
                                               "<b>Электронная</b> — подходит для большинства случаев и делается практически мгновенно\n\n"
                                               "<b>Бумажная</b> — делается долго, потому что на нее ставится живая подпись с печатью "
                                               "(заказывайте только если уверены, что нужна именно бумажная)",
                              parse_mode='HTML', reply_markup=create_response_markup_type())
-
-
-        @bot.message_handler(commands=['cancel'])
-        def cancel_message(message):
-            logging.info("Cancel message from user", extra={"user": message.from_user.username})
-            bot.send_message(message.chat.id, "Отменено")
-            bot.register_next_step_handler(message, welcome_message)
 
 
         def create_response_markup_type():
@@ -58,6 +40,11 @@ while True:
 
 
         def get_full_name(message, isDigital):
+            if message.text == "/cancel":
+                logging.info(f"Cancel message from user @{message.from_user.username}")
+                bot.send_message(message.chat.id, "Отменено")
+                return
+
             full_name = message.text
             bot.send_message(message.chat.id,
                              "Хорошо! Теперь, пожалуйста, введите причину для справки в формате «принимает участие в ...»")
@@ -65,6 +52,11 @@ while True:
 
 
         def get_reason(message, full_name, isDigital):
+            if message.text == "/cancel":
+                logging.info(f"Cancel message from user @{message.from_user.username}")
+                bot.send_message(message.chat.id, "Отменено")
+                return
+
             start_length = len("принимает участие в")
             try:
                 if len(message.text) <= start_length:
@@ -83,6 +75,11 @@ while True:
 
 
         def get_date(message, full_name, reason, isDigital):
+            if message.text == "/cancel":
+                logging.info(f"Cancel message from user @{message.from_user.username}")
+                bot.send_message(message.chat.id, "Отменено")
+                return
+
             words = "".join(c for c in message.text.replace(".", " ") if c.isalnum() or c == " ").strip().split()
             year = current_year = int(datetime.date.today().strftime("%Y"))
 
@@ -104,8 +101,7 @@ while True:
             except Exception as e:
                 bot.send_message(message.chat.id, f"Что-то не то с датой, попробуйте еще раз\n\n<code>{e}</code>",
                                  parse_mode='HTML')
-                bot.register_next_step_handler(message, get_date, full_name=full_name, reason=reason,
-                                               isDigital=isDigital)
+                bot.register_next_step_handler(message, get_date, full_name=full_name, reason=reason, isDigital=isDigital)
             else:
                 date = f"{day} {month} {year} года"
 
@@ -120,10 +116,11 @@ while True:
                                  f"{message.chat.id} {int(time.time())} {id_of_message_in_channel}\n\n"
                                  f"{info_text}\n\n{user_message}",
                                  parse_mode='HTML', reply_markup=create_response_markup_approval())
-                logging.info("Send request", extra={"user": message.from_user.username})
+                logging.info(f"Send request from user @{message.from_user.username}")
 
                 bot.send_message(message.chat.id,
-                                 "Спасибо! Справка отправлена на согласование, нужно немного подождать")
+                                 f"Спасибо! Справка отправлена на согласование, нужно немного подождать "
+                                 f"(примерно {beautiful_time(count_average_time(isDigital)).split('. ')[0]}.)")
 
 
         def create_response_markup_approval():
@@ -146,59 +143,33 @@ while True:
 
             elif call.data == "Reject":
                 id_of_user = call.message.text.split('\n')[0].split(' ')[0]
+                start_time = int(call.message.text.split('\n')[0].split(' ')[1])
                 id_of_message_in_channel = call.message.text.split('\n')[0].split(' ')[2]
+                isDigital = True if (call.message.text.split('Тип: '))[1].split('\n')[0] == "электронная" else False
+
                 bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                                       text=call.message.text + "\n\n❌ Отклонено")
                 bot.send_message(id_of_user, "К сожалению, справка не была согласована 🤷‍")
-                logging.info("Reject request", extra={"user": bot.get_chat(id_of_user).username})
+                logging.info(f"Reject request from user @{bot.get_chat(id_of_user).username}")
 
-                bot.edit_message_text(chat_id=CHANNEL_ID, message_id=id_of_message_in_channel,
-                                      text="📨"+call.message.text.split('📨')[1] + "\n\n❌ Отклонено")
+                bot.delete_message(chat_id=CHANNEL_ID, message_id=id_of_message_in_channel)
+                count_average_time(isDigital, int(time.time() - start_time))
+
             elif call.data == "Approve":
                 id_of_user = call.message.text.split('\n')[0].split(' ')[0]
+                start_time = int(call.message.text.split('\n')[0].split(' ')[1])
                 id_of_message_in_channel = call.message.text.split('\n')[0].split(' ')[2]
+
                 bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                                       text=call.message.text + "\n\n✅ Одобрено")
                 bot.send_message(id_of_user, "Ура, справка согласована 🎉")
-                logging.info("Approve request", extra={"user": bot.get_chat(id_of_user).username})
+                logging.info(f"Approve request from user @{bot.get_chat(id_of_user).username}")
 
                 full_name = (call.message.text.split('ФИО: '))[1].split('\n')[0]
                 reason = (call.message.text.split('Причина: '))[1].split('\n')[0]
                 date = (call.message.text.split('Дата: '))[1].split('\n')[0]
 
                 isDigital = True if (call.message.text.split('Тип: '))[1].split('\n')[0] == "электронная" else False
-
-                def create_PDF_certificate(full_name, reason, date, isDigital):
-                    # Open the Word document
-                    if isDigital:
-                        document = Document("original_file.docx")
-                    else:
-                        document = Document("original_file_paper.docx")
-
-                    today = datetime.date.today().strftime("%d.%m.%Y")
-                    # Modify the document
-                    document.paragraphs[
-                        3].text = f"От «{today.split('.')[0]}» {months[int(today.split('.')[1]) - 1]} {today.split('.')[2]} г."
-                    document.paragraphs[10].text = document.paragraphs[10].text.replace('Иванов Иван Иванович',
-                                                                                        full_name)
-                    document.paragraphs[10].text = document.paragraphs[10].text.replace('1 декабря 2022 года', date)
-                    document.paragraphs[10].text = document.paragraphs[10].text.replace('приминает участие в', reason)
-
-                    # Save the changes to the document
-                    document.save("edited_file.docx")
-
-                    # Convert .docx to .pdf
-                    def generate_pdf(doc_path, path):
-                        subprocess.call(['soffice',
-                                         # '--headless',
-                                         '--convert-to',
-                                         'pdf',
-                                         '--outdir',
-                                         path,
-                                         doc_path])
-                        return doc_path
-
-                    generate_pdf("edited_file.docx", "")
 
                 create_PDF_certificate(full_name, reason, date, isDigital)
 
@@ -215,58 +186,50 @@ while True:
 
                     # Send modified file to the user
                     with open(result_filename, "rb") as f:
-                        seconds = int(time.time() - int(call.message.text.split('\n')[0].split(' ')[1]))
-                        h = seconds // 3600
-                        m = seconds % 3600 // 60
-                        s = seconds % 3600 % 60
-                        if h > 0:
-                            preparation_time = '{} ч. {} мин. {} сек.'.format(h, m, s)
-                        elif m > 0:
-                            preparation_time = '{} мин. {} сек.'.format(m, s)
-                        elif s > 0:
-                            preparation_time = '{} сек.'.format(s)
-
-                        # time.sleep(10) # wait for the file to be saved
-
-                        # id_of_file = bot.send_document(CHANNEL_ID, f, caption=f"Сделано за {preparation_time}").document.file_id
                         bot.send_document(id_of_user, f)
-                        logging.info("Send to user", extra={"user": bot.get_chat(id_of_user).username})
+                        logging.info(f"Send to user @{bot.get_chat(id_of_user).username}")
 
-                        bot.edit_message_text(chat_id=CHANNEL_ID, message_id=id_of_message_in_channel,
-                                              text="📨" + call.message.text.split('📨')[1] + f"\n\n✅ Сделано ({preparation_time})")
+                    bot.delete_message(chat_id=CHANNEL_ID, message_id=id_of_message_in_channel)
+                    count_average_time(isDigital, int(time.time() - start_time))
                 else:
                     bot.send_message(id_of_user, "Уже печатаем справку. Сообщим, как только она будет готова 👌")
-                    # result_filename = "НА ПЕЧАТЬ - " + full_name + ".pdf"
-                    result_filename = "edited_file.pdf"
+                    result_filename = "НА ПЕЧАТЬ - " + full_name + ".pdf"
+                    os.rename("edited_file.pdf", result_filename)
                     with open(result_filename, "rb") as f:
                         bot.send_document(PRINTER_CHAT_ID, f,
-                                          caption=id_of_user + " " + call.message.text.split('\n')[0].split(' ')[1]
+                                          caption=id_of_user + " " + str(start_time) + " " + id_of_message_in_channel
                                                   + f"\n\nПечать справки для {full_name}",
                                           reply_markup=telebot.types.InlineKeyboardMarkup().add(
-                                              telebot.types.InlineKeyboardButton("🖨️ Напечатано",
-                                                                                 callback_data="Printed")))
+                                              telebot.types.InlineKeyboardButton("🖨️ Напечатано", callback_data="Printed")))
 
                 # Сlean up after ourselves
                 os.remove(result_filename)
 
             elif call.data == "Printed":
+                id_of_user = call.message.caption.split('\n')[0].split(' ')[0]
+                start_time = int(call.message.caption.split('\n')[0].split(' ')[1])
+                id_of_message_in_channel = call.message.caption.split('\n')[0].split(' ')[2]
+
                 bot.edit_message_caption(chat_id=call.message.chat.id, message_id=call.message.message_id,
                                          caption=call.message.caption + "\n\n🖨️ Напечатано")
-                bot.send_message(call.message.caption.split('\n')[0], f"Cправка для " +
-                                 (call.message.caption.split('Печать справки для '))[1].split('\n')[0] +
-                                 f" готова, можно забирать в {PLACE}")
-                logging.info("Send to user", extra={"user": bot.get_chat(call.message.caption.split('\n')[0]).username})
+                bot.send_message(id_of_user,
+                                 "Cправка для " + (call.message.caption.split('Печать справки для '))[1].split('\n')[0]
+                                 + " готова, можно забирать в " + PLACE)
+                logging.info("Send to user @" + bot.get_chat(id_of_user).username)
+
+                bot.delete_message(chat_id=CHANNEL_ID, message_id=id_of_message_in_channel)
+                count_average_time(False, int(time.time() - start_time))
 
 
-        logging.info("Bot running..", extra={"user": 0})
+        logging.info("Bot running...")
         bot.polling(none_stop=True)
 
     except Exception as e:
-        logging.error(e, extra={"user": 0})
+        logging.error(e)
         bot.stop_polling()
 
         time.sleep(15)
 
-        logging.info("Running again!", extra={"user": 0})
+        logging.info("Running again!")
 
 # bot.polling()
---
+++ utils.py
@@ -0,0 +1,77 @@
+from docx import Document
+import datetime
+import subprocess
+
+months = ["января", "февраля",
+          "марта", "апреля", "мая",
+          "июня", "июля", "августа",
+          "сентября", "октября", "ноября",
+          "декабря"]
+
+
+def create_PDF_certificate(full_name, reason, date, isDigital):
+    # Open the Word document
+    if isDigital:
+        document = Document("original_file.docx")
+    else:
+        document = Document("original_file_paper.docx")
+
+    today = datetime.date.today().strftime("%d.%m.%Y")
+    # Modify the document
+    document.paragraphs[
+        3].text = f"От «{today.split('.')[0]}» {months[int(today.split('.')[1]) - 1]} {today.split('.')[2]} г."
+    document.paragraphs[10].text = document.paragraphs[10].text.replace('Иванов Иван Иванович',
+                                                                        full_name)
+    document.paragraphs[10].text = document.paragraphs[10].text.replace('1 декабря 2022 года', date)
+    document.paragraphs[10].text = document.paragraphs[10].text.replace('приминает участие в', reason)
+
+    # Save the changes to the document
+    document.save("edited_file.docx")
+
+    generate_pdf("edited_file.docx", "")
+
+
+# Convert .docx to .pdf
+def generate_pdf(doc_path, path):
+    subprocess.call(['soffice',
+                     # '--headless',
+                     '--convert-to',
+                     'pdf',
+                     '--outdir',
+                     path,
+                     doc_path])
+    return doc_path
+
+
+def count_average_time(isDigital, current_time=0):
+    if isDigital:
+        filename = "digital_time.txt"
+    else:
+        filename = "paper_time.txt"
+
+    with open(filename, "r") as tf:
+        tf_content = tf.read().split('\n')
+        avegage_time = int(float(tf_content[0]))
+        number_of_requests = int(tf_content[1])
+
+    if current_time == 0:
+        return avegage_time
+
+    avegage_time = (avegage_time * number_of_requests + current_time) / (number_of_requests + 1)
+    number_of_requests += 1
+    with open(filename, "w") as tf:
+        tf.write(str(avegage_time) + '\n' + str(number_of_requests))
+
+
+def beautiful_time(seconds):
+    h = seconds // 3600
+    m = seconds % 3600 // 60
+    s = seconds % 3600 % 60
+    if h > 0:
+        preparation_time = '{} ч. {} мин. {} сек.'.format(h, m, s)
+    elif m > 0:
+        preparation_time = '{} мин. {} сек.'.format(m, s)
+    elif s > 0:
+        preparation_time = '{} сек.'.format(s)
+
+    return preparation_time
